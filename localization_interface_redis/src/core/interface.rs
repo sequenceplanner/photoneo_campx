f906b4935e2a864d69cf0570494bc1d75a6e3b87
endre90 <endre90@gmail.com>
2025-07-24 09:34:01 +0200

    transformation matrix try fix 2

--- localization_interface_redis/src/core/interface.rs
+++ localization_interface_redis/src/core/interface.rs
@@ -423,14 +423,15 @@ pub fn make_transforms(matrices: &[(MatrixDataInternal, String)], scanning_frame
         //     m[0][0], m[1][0], m[2][0], m[3][0], m[0][1], m[1][1], m[2][1], m[3][1], m[0][2],
         //     m[1][2], m[2][2], m[3][2], m[0][3], m[1][3], m[2][3], m[3][3],
         // );
-        let transformation_matrix = Matrix4::new(
-            m[0][0], m[0][1], m[0][2], m[0][3], m[1][0], m[1][1], m[1][2], m[1][3], m[2][0],
-            m[2][1], m[2][2], m[2][3], m[3][0], m[3][1], m[3][2], m[3][3],
-        );
+        // let transformation_matrix = Matrix4::new(
+        //     m[0][0], m[0][1], m[0][2], m[0][3], m[1][0], m[1][1], m[1][2], m[1][3], m[2][0],
+        //     m[2][1], m[2][2], m[2][3], m[3][0], m[3][1], m[3][2], m[3][3],
+        // );
+        let transformation_matrix = Matrix4::from(*matrix_array);
 
         log::info!(target: "phoxi_localization_interface", "Processing matrix for {}: {:?}", name, transformation_matrix);
 
-        let translation_vec = Vector3::new(m[0][3], m[1][3], m[2][3]);
+        let translation_vec = transformation_matrix.column(3).xyz(); //Vector3::new(m[0][3], m[1][3], m[2][3]);
 
         let rotation_matrix = transformation_matrix.fixed_view::<3, 3>(0, 0).into_owned();
 
